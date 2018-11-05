084c81898204c86beb1ecc67c2aa33be7dce6f05
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-11-05 11:50:48 +0300

    config express public dir and hbs

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const express = require('express'),
       hbs     = require('hbs');
 
 var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials'); //Let hbs use partials
+app.set('view engine', 'hbs'); // Set express to use hbs in the views dir
+
+app.use(express.static(__dirname + '/public')); //let express use /public folder

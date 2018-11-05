de12b56b4b60efdeceb1ff16995803194f924f3b
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-11-05 11:52:35 +0300

    add listen to app & port

--- server.js
+++ server.js
@@ -2,9 +2,15 @@
 const express = require('express'),
       hbs     = require('hbs');
 
+const port = process.env.PORT || 5000; 
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials'); //Let hbs use partials
 app.set('view engine', 'hbs'); // Set express to use hbs in the views dir
 
 app.use(express.static(__dirname + '/public')); //let express use /public folder
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
